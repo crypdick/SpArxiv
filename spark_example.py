fc2cd1d1f78654cf7c1102fe93be4a6aaf30c72e
SJCaldwell <shane.caldwell12@ncf.edu>
2018-04-23 09:30:03 -0400

    Want to make sure we can run Lepinski's test code

---
+++ spark_example.py
@@ -0,0 +1,24 @@
+from pyspark import SparkContext
+sc = SparkContext("local[*]", "App Name")
+
+def split_line(line):
+    strings = line.split()
+    return (strings[0], strings[1], int(strings[2]), int(strings[3]))
+
+def extract_population(data_point):
+    return (data_point[3], 1)
+
+def add_pops(pop_pairA, pop_pairB):
+    popA = pop_pairA[0]
+    countA = pop_pairA[1]
+    popB = pop_pairB[0]
+    countB = pop_pairB[1]
+    return (popA + popB, countA + countB)
+
+line_set = sc.textFile("./states.csv")
+split_lines = line_set.map(split_line)
+pop_pairs = split_lines.map(extract_population)
+total_pop, total_count = pop_pairs.reduce(add_pops)
+
+print("The average population is:")
+print(total_pop/total_count)
