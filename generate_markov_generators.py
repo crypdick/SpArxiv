c34a88f3b06a0378478c98b5c39689b8c1982bac
Richard Decal <richard.decal@ncf.edu>
2018-05-06 16:02:51 -0400

    combining models boilerplate

--- generate_markov_generators.py
+++ generate_markov_generators.py
@@ -1,4 +1,6 @@
 from pyspark import SparkContext
+import markovify
+
 sc = SparkContext("local[*]", "App Name")
 
 def split_line(line):
@@ -23,7 +25,6 @@ total_pop, total_count = pop_pairs.reduce(add_pops)
 print("The average population is:")
 print(total_pop/total_count)
 
-import markovify
 
 # Get raw text as string.
 with open("/path/to/my/corpus.txt") as f:
@@ -39,3 +40,9 @@ for i in range(5):
 # Print three randomly-generated sentences of no more than 140 characters
 for i in range(3):
     print(text_model.make_short_sentence(140))
+
+# combinging models
+model_a = markovify.Text(text_a)
+model_b = markovify.Text(text_b)
+
+model_combo = markovify.combine([ model_a, model_b ], [ 1.5, 1 ])
