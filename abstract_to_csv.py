348f02e3b724983ae7c6a73781a3b1fd8f18ddd7
Richard Decal <richard.decal@ncf.edu>
2018-05-06 21:21:09 -0400

    modifying Shane's parser to print on single lines TODO perl script

--- abstract_to_csv.py
+++ abstract_to_csv.py
@@ -1,31 +1,39 @@
 import csv
 from TexSoup import TexSoup
 import os
+import subprocess, sys
 
 
 def main():
     file_abstract = []
-    path = os.listdir('/Akamai_scratch/SpArxiv')
+    #path = os.listdir('/Akamai_scratch/SpArxiv')
+    path = os.listdir('/home/shit/bin/ds_shit/distributed/SpArxiv')
     indir = '/Akamai_scratch/arxiv/outdir3'
+    indir = "/home/shit/bin/ds_shit/distributed/SpArxiv"
     for root, dirs, filenames in os.walk(indir):
         for f in filenames:
-            try:
-                #open file
-                soup = TexSoup(open(os.path.join(root,f)))
-                #find abstract
-                abstract = str(soup.find('abstract'))
-                if abstract is not None:
-                    # delete Latex formatting
-                    # credit: https://tex.stackexchange.com/a/102404/100323
-                    
-                    # make abstract one line before append
-                    abstract = abstract.replace('\n', ' ')\
-                        .replace('\r', '')
+            if ".tex" in f:
+                try:
+                    #open file
+                    soup = TexSoup(open(os.path.join(root,f)))
+                    #find abstract
+                    abstract = str(soup.find('abstract'))
+                    if abstract is not None:
+                        # delete Latex formatting
+                        # credit: https://tex.stackexchange.com/a/102404/100323
+                        result = subprocess.run(
+                            [os.path.join(path, "removelatexcode.pl"), abstract], stdout=subprocess.PIPE)
+                        print(result.stdout)
+                        print("jo")
+
+                        # make abstract one line before append
+                        abstract = abstract.replace('\n', ' ')\
+                            .replace('\r', '')
 
 
-                    file_abstract.append((f, abstract))
-            except:
-                pass
+                        file_abstract.append((f, abstract))
+                except:
+                      pass
     with open('all_abstracts.csv','w') as out:
         csv_out=csv.writer(out)
         csv_out.writerow(['file','abstract'])
