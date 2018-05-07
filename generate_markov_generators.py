09307fe9dfc200f15ada1b84342c401fd9bb5dbb
Richard Decal <richard.decal@ncf.edu>
2018-05-06 23:06:26 -0400

    pep8

--- generate_markov_generators.py
+++ generate_markov_generators.py
@@ -16,20 +16,23 @@ def split_line(line):
     except:
         pass
 
+
 def at_least_20_words(text):
     try:
         return len(text.split()) >= 20
     except:  # for some reason we have some None's here
         pass
 
+
 def text_to_model(text):
     '''given an abstract, train a markov model
 
     the 1 will be used for weights, later'''
-    text_model = markovify.Text(text, state_size=STATE_SIZE, retain_original=False)
+    text_model = markovify.Text(text, state_size=STATE_SIZE,
+                                retain_original=False)
     # class is not serializable, so extract json first
     model_json = text_model.to_json()
-    return ("_", (model_json, 1))
+    return "_", (model_json, 1)
 
 
 def combine_models(model1_tup, model2_tup):
@@ -40,7 +43,8 @@ def combine_models(model1_tup, model2_tup):
     model2 = markovify.Text.from_json(model2_json)
     combined_model = markovify.combine([model1, model2], [weight1, weight2])
     combined_weight = weight1 + weight2
-    return ("_", (combined_model, combined_weight))
+    return "_", (combined_model, combined_weight)
+
 
 def model_to_json(model):
     jsonified = model.to_json()
@@ -65,4 +69,3 @@ models = models.reduce(combine_models)
 # # Print three randomly-generated sentences of no more than 140 characters
 # for i in range(3):
 #     print(text_model.make_short_sentence(140))
-
