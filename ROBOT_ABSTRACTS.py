58a872b6d4a82d5577982824dc3d24d3e8c0a6ca
Richard Decal <richard.decal@ncf.edu>
2018-05-07 15:29:22 -0400

    generating abstracts B-)

--- ROBOT_ABSTRACTS.py
+++ ROBOT_ABSTRACTS.py
@@ -1,16 +1,22 @@
 import markovify
-#import easygui
-
-#path = easygui.fileopenbox()
-
 import tkinter as tk
 from tkinter import filedialog
-
 root = tk.Tk()
 root.withdraw()
 
 print("PICK A MODEL from models folder")
-file_path = filedialog.askopenfilename()
+saved_model_path = filedialog.askopenfilename()
+print(saved_model_path)
+NUMBER_SENTENCES_TO_GENERATE = 5
+
+with open(saved_model_path, 'r') as f:
+    model_json = f.read()
+    reconstituted_model = markovify.Text.from_json(model_json)
+
+    for i in range(NUMBER_SENTENCES_TO_GENERATE):
+        print(reconstituted_model.make_sentence())
+
+
 
 
 #with open("./models/all_abstracts-RICHARD.csv") as abstracts:
